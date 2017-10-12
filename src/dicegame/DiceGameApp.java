5b5ce195a0dbdd9309a33260afcbd72cd85e6efa
TM <TM@CPH-R90BKTCZ.efif.dk>
2017-10-12 15:37:17 +0200

    Ib player added

--- src/dicegame/DiceGameApp.java
+++ src/dicegame/DiceGameApp.java
@@ -6,7 +6,7 @@ public class DiceGameApp {
 
     public static void main(String[] args) {
         ArrayList<Player> players = new ArrayList<>();      
-        players.add(new Human("Ib"));
+        players.add(new Human("Ibbb"));
         players.add(new AI("Computer"));       
         Game game = new Game(players, 8, 6, 100);
         game.play();
